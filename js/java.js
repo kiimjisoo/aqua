1d528576c3a6be2f5bdbccd1db08d970f98b04c0
kiimjisoo <jasoo22@naver.com>
2023-04-21 19:03:23 +0900

    0421
    swiper

---
+++ js/java.js
@@ -0,0 +1,22 @@
+
+
+
+// new Swiper('.main-slide .swiper', {
+
+//   direction: 'horizental',
+//   loop: true,
+//   autoplay: {
+//     delay: 4000
+//   },
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   centerSlides: true,
+//   pagintion: {
+//     el: '.main-slide .swiper-pagination',
+//     clickablw: true
+//   },
+//   navigation: {
+//     nextEl: '.main-slide .swiper-button-next',
+//     prevEl: '.main-slide .swiper-button-prev',
+//   },
+// });
