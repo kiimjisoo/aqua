ee99fdd09cfb114d74be8a714858b198f9700605
jisoo <jasoo22@naver.com>
2023-04-23 14:22:31 +0900

    0422
    이미지 슬라이드
    유튜브 삽입

--- js/java.js
+++ js/java.js
@@ -1,22 +1,20 @@
+const swiper = new Swiper('.main-slide .swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
 
-
-
-// new Swiper('.main-slide .swiper', {
-
-//   direction: 'horizental',
-//   loop: true,
-//   autoplay: {
-//     delay: 4000
-//   },
-//   slidesPerView: 1,
-//   spaceBetween: 10,
-//   centerSlides: true,
-//   pagintion: {
-//     el: '.main-slide .swiper-pagination',
-//     clickablw: true
-//   },
-//   navigation: {
-//     nextEl: '.main-slide .swiper-button-next',
-//     prevEl: '.main-slide .swiper-button-prev',
-//   },
-// });
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
