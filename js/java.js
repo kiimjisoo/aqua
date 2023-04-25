9f76cd4772d0b677cd54193404dc33de791f0e7e
kiimjisoo <jasoo22@naver.com>
2023-04-25 15:17:20 +0900

    0425
    공지슬라이드 수정
    transition  추가

--- js/java.js
+++ js/java.js
@@ -1,37 +1,40 @@
-const swiper = new Swiper('.main-slide .swiper', {
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 5000
-    },
 
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
 
-// const swiper1 = new Swiper('.notice .swiper', {
-//     direction: 'vertical',
-//     loop: true,
-//     autoplay: true,
-// });  
 
+
+new Swiper('.notice .swiper', {
+  direction: 'vertical',
+  loop: true,
+  autoplay: true,
+}); 
+
+
+const swiperel = new Swiper('.main-slide .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
 
 const spyEls = document.querySelectorAll('section.scroll-spy');
 spyEls.forEach(function (spyEls) {
-  new ScrollMagic
-    .Scene({ 
-      triggerElement: spyEls, 
-      triggerHook: 0.1
-    })
-    .setClassToggle(spyEls, 'show')
-    .addTo(new ScrollMagic.Controller()); 
+new ScrollMagic
+  .Scene({ 
+    triggerElement: spyEls, 
+    triggerHook: 0.8
+  })
+  .setClassToggle(spyEls, 'show')
+  .addTo(new ScrollMagic.Controller()); 
 });
 
